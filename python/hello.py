1c495fb3bf6ea6703317a11d945f00c5fafadeac
Maryna <marinakrasnovatr81@gmail.com>
2020-09-05 14:31:25 +0300

    add python example

---
+++ python/hello.py
@@ -0,0 +1,23 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+print("Hello world")
+
+# Python program to demonstrate working 
+# of map. 
+  
+# Return double of n 
+def addition(n): 
+    return n + n 
+    
+numbers = (1, 2, 3, 4)
+  
+# We double all numbers using map() 
+print(addition(1)) 
+
+print(list(numbers))
+
+result = map(addition, numbers)
+print(list(result))
+
+
